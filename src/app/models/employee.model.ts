db29b46641cd5ad90691ec986f12f6fe591b7aaa
Muata Nkosi <mnkosi@fandm.edu>
2018-07-21 16:13:05 -0400

    creating a model object

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,10 @@
+import { getTypeNameForDebugging } from "../../../node_modules/@angular/common/src/directives/ng_for_of";
+
+export class Employee{
+    constructor(
+        public firstName: string,
+        public lastName: string
+    ){
+
+    }
+}
